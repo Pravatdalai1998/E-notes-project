b1905c1615782f88534f876d2e1de9d322ef64ac
Pravat Kumar <pravatofficial1998@gmail.com>
2025-05-05 00:52:22 +0530

    Initial commit

---
+++ src/main/java/com/Enotes/Enotes_Api_service/EnotesApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Enotes.Enotes_Api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesApiServiceApplication.class, args);
+	}
+
+}
